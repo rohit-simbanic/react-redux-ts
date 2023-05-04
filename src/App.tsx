9e816a6221b23a1925c78de0c5284b3758f3c7ff
Rohit Mondal <rray524@gmail.com>
2023-05-04 12:55:01 +0530

    navbar code done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
+import Watch from "./Pages/Watch";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
